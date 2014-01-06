2d04ba48bd118745b481bdac8e93c614efcb3a71
Endare <verschueren.sam@endare.eu>
2014-01-06 17:53:27 +0100

    -

--- src/PushNotificationPlugin.cs
+++ src/PushNotificationPlugin.cs
@@ -1,12 +1,14 @@
+using System;
 using WPCordovaClassLib.Cordova;
 using WPCordovaClassLib.Cordova.Commands;
 using WPCordovaClassLib.Cordova.JSON;
 
-namespace eu.endare.phonegap
+namespace WPCordovaClassLib.Cordova.Commands
 {
     public class PushNotificationPlugin : BaseCommand
     {
-        public void add(String test) {
+        public void add(String test) 
+        {
             DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "Everything went as planned, this is a result that is passed to the success handler."));
         }
     }
